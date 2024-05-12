5866f91dc96ba806b098f3d4c3df45a823427ae6
SamSkull19 <sifatsamin19@gmail.com>
2024-05-12 20:52:35 +0600

    apply modal close updated

--- src/JobByCategory/JobByCategory.jsx
+++ src/JobByCategory/JobByCategory.jsx
@@ -48,7 +48,7 @@ const JobByCategory = () => {
 
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                     {
-                        filteredJobs.map(job => <JobBySingleCategory key={jobs.id} job={job}></JobBySingleCategory>)
+                        filteredJobs.slice(0,9).map(job => <JobBySingleCategory key={jobs.id} job={job}></JobBySingleCategory>)
                     }
                 </div>
             </div>
--- src/JobDetails/JobDetails.jsx
+++ src/JobDetails/JobDetails.jsx
@@ -173,7 +173,8 @@ const JobDetails = () => {
 
                                         <div className="modal-action">
                                             <form method="dialog">
-                                                <button className="btn">Close</button>
+                                                {/* if there is a button in form, it will close the modal */}
+                                                <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                                             </form>
                                         </div>
 
